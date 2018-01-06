e3abe9ccd1e80eed3f6bc6cbf7532ac5c2369434
Karthi <31033477+karthi1998@users.noreply.github.com>
2018-01-06 11:22:10 +0530

    Add files via upload

---
+++ police.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,l,i,j,count;
+	cout<<"Enter the values of n and k";
+	cin>>n l;
+	cout<<"\nEnter 1 for Police & 0 for theif\n";
+	int a[10][10];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			
+            if(a[i][j]==1)
+			{
+             for(k=1;k<=l;k++)
+			{
+                 if(a[i][j+k]==0)
+                 {
+                     count++;
+                     a[i][j+k]=2;
+                     a[i][j]=2;
+                 
+                 }
+			     else if(a[i][j-k]==0)
+			     {
+                      count++;
+                      a[i][j-k]==2;
+                      a[i][j]==2;
+                 }
+                 else{}
+                 }
+                 
+		}
+	}
+	cout<<endl<<count;
+	return 0;
+}
